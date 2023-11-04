a5beb3a304e35f01657d8692809d227f59c61c5f
Kalie <kwatsonnov2002@gmail.com>
2023-11-03 21:01:13 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+customerName = "bob"
+
+function getCustomerName() {
+    return customerName;
+}
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer() {
+    throw new Error("Assignment to constant variable.");
+  }
